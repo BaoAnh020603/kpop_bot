2df65737071063acab343f8c962409401e1ad828
Nguyễn Nhật Bảo Anh <143186266+BaoAnh020603@users.noreply.github.com>
2025-10-26 22:32:01 +0700

    Add files via upload

---
+++ start_kpop_bot.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "C:\Users\ADMIN\OneDrive\Desktop\Desktop\Downloads\kpop_bot"
+
+:: Thay YOUR_BOT_TOKEN_HERE bằng token thực của bot
+set DISCORD_TOKEN=MTQzMTUyMjQ1MDg0MzExMTQ1NA.GY9cvE.mKzNEdiROx2d-t2phSVhvPdMXAMhbvakCQVNyM
+
+echo Đang chạy KPop Bot...
+"C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.11_3.11.2544.0_x64__qbz5n2kfra8p0\python3.11.exe" kpop_radio_bot.py
+
+pause
